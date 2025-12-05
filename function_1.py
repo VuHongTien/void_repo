4b41ea598e4185226d889154c30be30e193c039e
VuHongTien <vuhongtien98@gmail.com>
2025-12-05 15:33:09 +0700

    remove function too_much_choices on function_1

--- function_1.py
+++ function_1.py
@@ -14,19 +14,11 @@ def print_or_not_print(chosse):
     else:
         print("You know what you did")
 
-#code for fun
 
-def too_much_choices(end):
-    if end:
-        print("this is the end")
-
-    else:
-        print("i still can handle it")
 
 #give this file a main()
 
 if __name__ == "__main__":
     print_something()
     print_or_not_print(True)
-    too_much_choices(False)
     #it's time to consider putting these functions into the main branch
