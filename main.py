af03596877b43a0e225695d033be92d1e9561373
VuHongTien <vuhongtien98@gmail.com>
2025-12-05 15:22:01 +0700

    update main() in main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from function_1 import print_something, print_or_not_print, too_much_choices
+from function_2 import deet
+
+if __name__ == "__main__":
+    #test function from function_1.py
+    print_something()
